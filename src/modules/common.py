eaed592279b54e18b95c9dab90521ab5a6a1e54b
papasim824 <example@example.com>
2023-07-28 21:55:57 +0900

    mod

--- src/modules/common.py
+++ src/modules/common.py
@@ -39,6 +39,12 @@ def hide_style():
                     [data-testid="collapsedControl"] {
                         display: none
                     }
+                    .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                    }
                     </style>
                     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
