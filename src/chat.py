70b36265423dc0396a8edc88423c312153df8d6c
ppspps824 <34480286+ppspps824@users.noreply.github.com>
2023-07-29 10:11:32 +0900

    chat.py を更新

--- src/chat.py
+++ src/chat.py
@@ -165,10 +165,12 @@ if st.session_state.name:
             action_list = [info for info in ai_list if f"@{info}" in user_msg]
         
         if st.session_state.brains_action=="カレント":
-            action_list.append([st.session_state.current_ai_name])
+            action_list.append(st.session_state.current_ai_name)
+            st.write(action_list)
         
         if st.session_state.brains_action=="ランダム":
-            action_list.append([random.sample(ai_list,random.randint(1,len(ai_list)))])
+            action_list.append(random.sample(ai_list,random.randint(1,len(ai_list))))
+            st.write(action_list)
         
         
         if action_list:
